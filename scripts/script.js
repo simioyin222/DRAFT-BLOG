690fe683c6428180c410ffa84f219fc6d3351ea5
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-15 21:30:16 -0600

    add username and post contnet ability

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
       createPost();
   };
 });
+
+function createPost() {
+  var username = document.getElementById('username').value.trim();
+  var content = document.getElementById('new-post-content').value.trim();
+  if (username && content) {
+      var postSection = document.createElement('section');
+      var currentDate = new Date().toLocaleDateString();
+      postSection.innerHTML = `
+          <article>
+              <header>
+                  <h3>Posted by ${username} on ${currentDate}</h3>
+              </header>
+              <p>${content}</p>
+          </article>
+      `;
+  }};
