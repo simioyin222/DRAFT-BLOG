528032d76f8afe174fa99994f41a6127b46b46fe
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-16 22:51:00 -0600

    updated app.js file

--- server/app.js
+++ server/app.js
@@ -1,21 +1,38 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
 const app = express();
 const port = 3000;
 
-const mongoDBConnectionString = process.env.MONGODB_URI;
-
-mongoose.connect(mongoDBConnectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
+// Middleware for parsing JSON and urlencoded data and for sessions
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'your session secret', // Replace with a real secret in production
+  resave: false,
+  saveUninitialized: false,
+}));
+
+// Initialize Passport and use it with sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static files
 app.use(express.static('../public'));
 
+// Routes
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// Listen on the specified port
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
