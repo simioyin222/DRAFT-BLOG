999494fa3d0628ab0d5a6494446f7a2eb910c023
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-16 22:33:57 -0600

    add express and mongoose , and official mongo connection string

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+const mongoDBConnectionString = process.env.MONGODB_URI;
+
+mongoose.connect(mongoDBConnectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.use(express.static('../public'));
 
