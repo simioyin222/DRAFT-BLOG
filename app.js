49fa3b1a28f54fd25d22692e43f1e05f5ba9c59c
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-18 11:11:07 -0600

    add comments models an specializattion of others

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
-const User = require('./server/models/User'); // Adjust path as necessary
-const Post = require('./server/models/Post'); // Adjust path as necessary
+const User = require('./server/models/User');
+const Post = require('./server/models/Post');
+const Comment = require('./server/models/Comment');
 
 const app = express();
 const port = 3000;
@@ -19,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(session({
-  secret: 'secret', // Replace with your secret in production
+  secret: 'secret',
   resave: true,
   saveUninitialized: true
 }));
@@ -34,10 +35,10 @@ passport.use(new LocalStrategy((username, password, done) => {
     }
     bcrypt.compare(password, user.password, (err, res) => {
       if (res) {
-        // Passwords match
+        
         return done(null, user);
       } else {
-        // Passwords do not match
+        
         return done(null, false, { message: 'Incorrect password.' });
       }
     });
@@ -49,32 +50,27 @@ passport.deserializeUser((id, done) => {
   User.findById(id, (err, user) => done(err, user));
 });
 
-// User Registration Route
 app.post('/register', (req, res) => {
   const { username, password } = req.body;
   bcrypt.hash(password, 10, (err, hashedPassword) => {
     if (err) throw err;
     new User({ username, password: hashedPassword }).save()
-      .then(user => res.redirect('/')) // Redirect to homepage after registration
+      .then(user => res.redirect('/')) 
       .catch(err => console.log(err));
   });
 });
 
-// User Login Route
 app.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login',
   failureFlash: false
 }));
 
-// User Logout Route
 app.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/login');
 });
 
-// Blog Post Routes
-// POST route to create a new blog post
 app.post('/posts', (req, res) => {
   if (!req.isAuthenticated()) {
     return res.status(401).send('User not authenticated');
@@ -89,14 +85,13 @@ app.post('/posts', (req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// GET route to retrieve all blog posts
 app.get('/posts', (req, res) => {
   Post.find()
+    .populate('author')
     .then(posts => res.json(posts))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// PUT route to update a specific blog post
 app.put('/posts/:postId', (req, res) => {
   if (!req.isAuthenticated()) {
     return res.status(401).send('User not authenticated');
@@ -115,13 +110,44 @@ app.put('/posts/:postId', (req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// DELETE route to delete a specific blog post
 app.delete('/posts/:postId', (req, res) => {
   if (!req.isAuthenticated()) {
     return res.status(401).send('User not authenticated');
   }
   Post.findByIdAndDelete(req.params.postId)
     .then(() => res.json('Post deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+app.post('/posts/:postId/comments', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).send('User not authenticated');
+  }
+  const newComment = new Comment({
+    content: req.body.content,
+    author: req.user._id,
+    post: req.params.postId
+  });
+  newComment.save()
+    .then(comment => res.json(comment))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+app.delete('/comments/:commentId', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).send('User not authenticated');
+  }
+  Comment.findById(req.params.commentId)
+    .then(comment => {
+      if (comment.author.toString() !== req.user._id.toString()) {
+        return res.status(403).send('Not authorized to delete this comment');
+      }
+      comment.remove()
+        .then(() => res.json('Comment deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
